6f7845bc2bd6bb641f6f1f34be4f041d49485a25
pplknomeasb <thompson.brennan@gmail.com>
2021-06-12 13:27:19 -0400

    Create a new branch after each commit for the next commit

---
+++ src/main/java/org/perscholas/HairAndNailApplication.java
@@ -0,0 +1,13 @@
+package org.perscholas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HairAndNailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HairAndNailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/perscholas/HairAndNailApplicationTests.java
@@ -0,0 +1,13 @@
+package org.perscholas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HairAndNailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
